88a143537ce6c3eab6975703366ae10bc0fd99c1
Vkin-7 <victorrealm@gmail.com>
2023-04-04 19:02:52 -0300

    fix: alter createUser migration

--- src/infra/data/database/migrations/1676746924212-CreateUsers.ts
+++ src/infra/data/database/migrations/1676746924212-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1676746924212 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'uuid'
+                        type: 'uuid',
+                        isPrimary: true,
                     },
                     {
                         name: 'name',
