2c64fb682150011c74a8cc3c86eb2cc8eb0fbdf5
Vkin-7 <victorrealm@gmail.com>
2023-06-19 20:30:15 -0300

    feat: update gitignore

---
+++ src/infra/data/database/data-source.example.ts
@@ -0,0 +1,20 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SeederOptions } from 'typeorm-extension';
+import { MainSeeder } from './seeds/MainSeeder';
+
+
+const options: DataSourceOptions & SeederOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'docker',
+    password: 'ignite',
+    database: 'rentx',
+    synchronize: false,
+    logging: false,
+    entities: ['./src/infra/data/entities/**/*.{ts,js}'],
+    migrations: ['./src/infra/data/database/migrations/*.{ts,js}'],
+    seeds: [MainSeeder]
+};
+
+export const AppDataSource = new DataSource(options);
